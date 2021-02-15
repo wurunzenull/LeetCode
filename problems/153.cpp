f62b2550ef27183734b91d4c4af9f3e72f0ec6e8
wurunzenull <181860109@smail.nju.edu.cn>
2021-02-15 22:46:12 +0800

    153:find-minimum-in-rotated-sorted-array done

---
+++ problems/153.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+};
+
+vector<int> readArrays() {
+    vector<int> nums;
+    string input;
+    getline(cin, input);
+    istringstream in(input);
+    int num;
+    while (in >> num) {
+        nums.push_back(num);
+    }
+    in.clear();
+    return nums;
+}
+
+int main() {
+    vector<int> nums = readArrays();
+    Solution mySolution;
+    cout << mySolution.findMin(nums) << endl;
+    return 0;
+}
