51bcf34e54936e55deca52ee07dcfefb73665e7e
wurunzenull <181860109@smail.nju.edu.cn>
2021-02-16 13:42:45 +0800

    39:combination-sum done

---
+++ problems/39.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    void findCombination(vector<vector<int>>& combinations, vector<int>& combination, vector<int>& candidates,
+                         int target, int start) {
+        if (target == 0) {
+            combinations.push_back(combination);
+            return;
+        }
+        for (int i = start; i < candidates.size(); i++) {
+            int candidate = candidates[i];
+            if (target < candidate) break;
+            combination.push_back(candidate);
+            findCombination(combinations, combination, candidates, target - candidate, i);
+            combination.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> combinations;
+        vector<int> combination;
+        sort(candidates.begin(), candidates.end());
+        findCombination(combinations, combination, candidates, target, 0);
+        return combinations;
+    }
+};
+
+vector<int> readArrays() {
+    vector<int> nums;
+    string input;
+    getline(cin, input);
+    istringstream in(input);
+    int num;
+    while (in >> num) {
+        nums.push_back(num);
+    }
+    in.clear();
+    return nums;
+}
+
+int main() {
+    vector<int> candidates = readArrays();
+    int target;
+    cin >> target;
+    Solution mySolution;
+    vector<vector<int>> combinations = mySolution.combinationSum(candidates, target);
+    for (auto combination : combinations) {
+        for (auto condidate : combination) {
+            cout << condidate << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
