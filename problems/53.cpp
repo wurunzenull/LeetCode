5debb4758f3299c008d3e506e116b3607c9d2445
wurunzenull <181860109@smail.nju.edu.cn>
2021-02-17 17:44:12 +0800

    [upload] 53:maximum-subarray 添加分治算法

--- problems/53.cpp
+++ problems/53.cpp
@@ -3,7 +3,32 @@ using namespace std;
 
 class Solution {
    public:
+    struct Status {
+        int lSum, rSum, mSum, iSum;
+    };
+
+    Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = max(l.lSum, l.iSum + r.lSum);
+        int rSum = max(r.rSum, r.iSum + l.rSum);
+        int mSum = max(max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return (Status){lSum, rSum, mSum, iSum};
+    }
+
+    Status get(vector<int>& nums, int l, int r) {
+        if (l == r) {
+            return (Status){nums[l], nums[l], nums[l], nums[l]};
+        }
+        int m = (l + r) >> 1;
+        Status lSub = get(nums, l, m);
+        Status rSub = get(nums, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
     int maxSubArray(vector<int>& nums) {
+        return get(nums, 0, nums.size() - 1).mSum;
+        /*
+         //贪心算法
         int res = INT32_MIN;
         int add = 0;
         for (auto num : nums) {
@@ -12,6 +37,7 @@ class Solution {
             if (add <= 0) add = 0;
         }
         return res;
+        */
     }
 };
 
