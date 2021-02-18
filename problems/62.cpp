6aed2f1a542661abb9abe6d8d15ed4f88d007187
wurunzenull <181860109@smail.nju.edu.cn>
2021-02-18 17:33:51 +0800

    [upload] 62:unique-paths 简单dp

--- problems/62.cpp
+++ problems/62.cpp
@@ -10,7 +10,7 @@ class Solution {
         return res;
     }
     int combination(int m, int n) { return factorial(m) / (factorial(n) * factorial(m - n)); }
-    int uniquePaths(int m, int n) {
+    int uniquePaths2(int m, int n) {
         long long res = 1;
         // if (m > n) return combination(m + n - 2, n - 1);
         // return combination(m + n - 2, m - 1);
@@ -18,6 +18,21 @@ class Solution {
             res = res * x / y;
         }
         return res;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> path(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            path[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            path[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                path[i][j] = path[i - 1][j] + path[i][j - 1];
+            }
+        }
+        return path[m - 1][n - 1];
     }
 };
 int main() {
