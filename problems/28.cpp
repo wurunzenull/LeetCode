09854e82c2b52f7e3bbb51bb1113e4303adbd306
wurunzenull <181860109@smail.nju.edu.cn>
2021-02-20 18:34:16 +0800

    [update] 28:implement-strstr 添加KMP算法实现

--- problems/28.cpp
+++ problems/28.cpp
@@ -3,6 +3,29 @@ using namespace std;
 
 class Solution {
    public:
+    vector<int> prefixFunction(string s) {
+        int n = s.length();
+        vector<int> pi(n, 0);
+        for (int i = 1; i < n; i++) {
+            int j = pi[i - 1];
+            while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+            if (s[i] == s[j]) j++;
+            pi[i] = j;
+        }
+        return pi;
+    }
+    int strStr2(string haystack, string needle) {
+        int m = haystack.length(), n = needle.length();
+        if (n == 0) return 0;
+        vector<int> next = prefixFunction(needle);
+        int j = next[0];
+        for (int i = 0; i < m; i++) {
+            while (j > 0 && haystack[i] != needle[j]) j = next[j - 1];
+            if (haystack[i] == needle[j]) j++;
+            if (j == n) return (i - n + 1);
+        }
+    }
+
     int strStr(string haystack, string needle) {
         int m = haystack.length(), n = needle.length();
         if (n == 0) return 0;
