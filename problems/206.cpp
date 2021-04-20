5f353525109820007add005715e571148663df2f
wurunzenull <181860109@smail.nju.edu.cn>
2021-04-20 10:33:16 +0800

    [add] 206:reverse-linked-list 反转链表.

---
+++ problems/206.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+class Solution {
+   public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* cur = head;
+        ListNode* next = NULL;
+        while (cur) {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+    ListNode* reverse(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* res = reverse(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return res;
+    }
+};
